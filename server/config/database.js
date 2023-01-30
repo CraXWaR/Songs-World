4106cbd2ecf7e3732de3183411aced0ce24319cb
unknown <craxwar@gmail.com>
2023-01-30 14:11:03 +0200

    Initialiazing database

---
+++ server/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+function initDatabase() {
+    return mongoose.connect(process.env.CONNECTIONSTRING);
+
+
+}
+
+module.exports = initDatabase;
