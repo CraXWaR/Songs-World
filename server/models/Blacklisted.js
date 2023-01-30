7abc9e980e8addce17947657e155a8bd53d81621
unknown <craxwar@gmail.com>
2023-01-30 14:40:16 +0200

    Adding blacklisted model

---
+++ server/models/Blacklisted.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const blacklistedSchema = new mongoose.Schema({
+    token: {
+        required: true,
+        type: String
+    }
+});
+
+const blacklisted = new mongoose.model('Blacklist', blacklistedSchema);
+
+module.exports = blacklisted;
