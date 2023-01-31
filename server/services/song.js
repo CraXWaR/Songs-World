23edfd6524ae8acb71e1820e069f826278d2433d
unknown <craxwar@gmail.com>
2023-01-31 14:22:51 +0200

    Formating the code and adding some TODO comments

--- server/services/song.js
+++ server/services/song.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 const addSong = async (song, id) => {
     try {
         song.owner = id;
-        return await Song.create({...song})
+        return await Song.create({ ...song })
     } catch (error) {
         console.log(error);
         throw new Error(error);
     }
 }
+
+//TODO more funcions for song CRUD operations
 
 module.exports = {
     addSong
