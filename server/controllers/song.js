08b54d2cb66415d4e2887a8a7a359d8f82f36c89
unknown <craxwar@gmail.com>
2023-02-01 09:21:25 +0200

    Creating funcion for adding songs

---
+++ server/controllers/song.js
@@ -0,0 +1,17 @@
+const { addSong } = require('../services/song');
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    const data = req.body;
+
+    try {
+        console.log(data);
+        const userId = req?.user?._id;
+        const song = await addSong(data, userId);
+        //TODO update songs added by users
+        res.status(201).json(song);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message});
+    }
+})
