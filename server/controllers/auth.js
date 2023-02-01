e117e9c312762343f59e8f91f9f0e6607d34bce3
unknown <craxwar@gmail.com>
2023-02-01 08:47:37 +0200

    Adding login funcionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -14,3 +14,17 @@ router.post('/register', async (req, res) => {
 
     res.end();
 })
+
+router.post('/login', async (req, res) => {
+    const {email, password } = req.body;
+
+    try {
+        const user = await login(email, password);
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
+    }
+
+    res.end();
+})
