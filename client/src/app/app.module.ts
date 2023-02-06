b424dbadc91a5826ccaf0fae7d9ffaeecf4d48bd
unknown <craxwar@gmail.com>
2023-02-06 13:35:52 +0200

    Fixing bug

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/home/home.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
