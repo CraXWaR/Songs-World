50420d63c74258c1afbfe2b8fae341ab3bac63d9
unknown <craxwar@gmail.com>
2023-02-06 14:02:26 +0200

    Creating footer component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class CoreModule { }
