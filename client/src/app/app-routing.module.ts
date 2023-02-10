8ccf7c4d24560a6e653051bc2ba2a78daee805c0
unknown <craxwar@gmail.com>
2023-02-10 12:35:18 +0200

    Adding routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './core/home/home.component';
+import { CatalogComponent } from './song/catalog/catalog.component';
+import { CreateComponent } from './song/create/create.component';
+import { ProfileComponent } from './song/profile/profile.component';
 
 const routes: Routes = [
   {
@@ -9,6 +14,41 @@ const routes: Routes = [
     component: HomeComponent,
     data: {
       title: 'Home Page'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register Page'
+    }
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+    data: {
+      title: 'Catalog Page'
+    }
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+    data: {
+      title: 'Create Page'
+    }
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    data: {
+      title: 'Profile Page'
     }
   }
 ];
