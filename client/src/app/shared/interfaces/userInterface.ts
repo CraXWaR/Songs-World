a73f29ce8ed6c6dcc1e9dc50fab1794a58b15e61
unknown <craxwar@gmail.com>
2023-02-17 11:41:11 +0200

    Updating user interface

---
+++ client/src/app/shared/interfaces/userInterface.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    username: string,
+    email: string,
+    accessToken: string,
+    _id: any,
+    songs: string[],
+    favouriteSongs: string[],
+    avatar: string,
+}
