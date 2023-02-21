7f3c554aae6f21003928701cbf3f303653752461
unknown <craxwar@gmail.com>
2023-02-21 12:33:20 +0200

    Creating password validator

--- client/src/app/shared/validators.ts
+++ client/src/app/shared/validators.ts
@@ -8,3 +8,14 @@ export function emailValidator(control: AbstractControl): ValidationErrors | nul
         invalidEmail: true
     }
 }
+
+export function passwordValidator(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+        return null;
+    }
+    const password = control.parent?.get('password')?.value;
+    const rePassword = control.value
+    return password == rePassword ? null : {
+        invalidPasswords: true
+    }
+}
