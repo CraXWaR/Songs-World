4238b08b60b799f2424dd40749bca7a682edf0bc
unknown <craxwar@gmail.com>
2023-02-22 13:40:29 +0200

    Removign warning from mogoose

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 function initDatabase() {
+    mongoose.set("strictQuery", false);
     return mongoose.connect('mongodb://0.0.0.0:27017/songsWorld');
 
 }
