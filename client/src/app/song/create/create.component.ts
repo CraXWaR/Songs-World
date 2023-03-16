75e0eec91b792e340596d2d5a4c7c0edcf9e948b
CraXWaR <craxwar@gmail.com>
2023-03-16 11:14:52 +0200

    commit

--- client/src/app/song/create/create.component.ts
+++ client/src/app/song/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SongService } from 'src/app/services/song.service';
 import { errorHandler } from 'src/app/shared/errorHandler';
@@ -11,19 +11,32 @@ import { errorHandler } from 'src/app/shared/errorHandler';
 })
 export class CreateComponent {
 
+  form!: FormGroup;
   errors: string | undefined = undefined;
 
-  constructor(private songService: SongService, private router: Router) { }
+  constructor(private fb: FormBuilder, private songService: SongService, private router: Router) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      genre: ['', [Validators.required]],
+      year: ['', [Validators.required]],
+      description: ['', [Validators.minLength(10), Validators.maxLength(500)]],
+      songImage: ['', [Validators.required]]
+    })
+  }
 
   async onCreate(form: NgForm) {
     let token = localStorage.getItem('token');
-    let value = form.value;
+    let value = this.form.value;
     value.token = token;
-
+    // console.log(this.form.value);
     this.songService.addSong(value).subscribe({
-      next: () => this.router.navigate(['/catalog']),
+      next: () => {
+        this.router.navigate(['/catalog']);
+      },
       error: (err) => {
-        this.errors = errorHandler(err?.error?.error);
+        console.log(err);
+        this.errors = errorHandler(err.error?.error);
       }
     })
   }
