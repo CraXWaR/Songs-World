1068e140d86752f35c37655c2c804c3e9c8372ba
CraXWaR <craxwar@gmail.com>
2023-04-04 12:23:12 +0300

    Creating funcion to get the user info

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { IUser } from 'src/app/shared/interfaces/userInterface';
 
 @Component({
   selector: 'app-profile',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  user: IUser | undefined;
 
+  constructor(private userService: UserService) {
+    this.getUserInfo();
+  }
+
+  getUserInfo() {
+    this.user = undefined;
+    this.userService.getUserData().subscribe({
+      next: (user) => {
+        this.user = user;
+      }, 
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 }
 
 
