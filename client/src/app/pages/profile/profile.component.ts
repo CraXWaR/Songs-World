eba8fa90cc9f59888428d7e4d08a0abb40a1be31
CraXWaR <craxwar@gmail.com>
2023-04-12 13:12:01 +0300

    Creating edit user funcion in profile

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { errorHandler } from 'src/app/shared/errorHandler';
 import { IUser } from 'src/app/shared/interfaces/userInterface';
+import { emailValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-profile',
@@ -10,14 +13,25 @@ import { IUser } from 'src/app/shared/interfaces/userInterface';
 export class ProfileComponent {
   user: IUser | undefined;
   onEdit: boolean = false;
+  form!: FormGroup;
+  errors: string | undefined = undefined;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      email: ['', [Validators.required, emailValidator]],
+      personalInfo: [''],
+      avatar: ['', [Validators.required]]
+    });
+
     this.getUserInfo();
   }
 
   getUserInfo() {
     let token = localStorage.getItem('token');
-    
+
     this.userService.getUserData({ token }).subscribe({
       next: (user) => {
         this.user = user
@@ -27,6 +41,22 @@ export class ProfileComponent {
 
       }
     });
+  }
+  onEditUser() {
+    const id = this.user?._id;
+    let token = localStorage.getItem('token');
+    let value = this.form.value;
+    value.token = token;
+
+    this.userService.editUser(id, value).subscribe({
+      next: (user) => {
+        this.user = user;
+        this.onEdit = false;
+      },
+      error: (err) => {
+        this.errors = errorHandler(err.error?.error);
+      }
+    })
   }
 }
 
